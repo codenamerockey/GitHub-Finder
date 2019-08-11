4cd745e5ed1ee3bec0fbe705f02bf0a2365d96ad
Rockey Simmons <codenamerockey@gmail.com>
2019-08-11 07:36:16 -0700

    updated state to come from App.js also installed axios with yarn and looping through data from github api add props to user components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  /* componentDidMount() {
+    // sending request for data with .then() promise
+    axios.get('https://api.github.com/users').then(response => {
+      console.log(response.data);
+    });
+  }*/
+  /********async await syntax for axios http request***********/
+  async componentDidMount() {
+    this.setState({ loading: true }); // this sets our loading to true before we get our data from the api
+
+    const resolve = await axios.get('https://api.github.com/users'); // get data
+
+    //after getting data we update the state
+    this.setState({ users: resolve.data, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
