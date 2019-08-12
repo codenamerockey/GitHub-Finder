992dc4643ea0f7e1b01413f5bb0eaf9f7e7d3cc8
Rockey Simmons <codenamerockey@gmail.com>
2019-08-11 18:38:19 -0700

    added search functionality and a onchange and on submit functionality to the search component file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 
@@ -17,20 +18,38 @@ class App extends Component {
     });
   }*/
   /********async await syntax for axios http request***********/
-  async componentDidMount() {
-    this.setState({ loading: true }); // this sets our loading to true before we get our data from the api
+  // async componentDidMount() {
+  //   this.setState({ loading: true }); // this sets our loading to true before we get our data from the api
 
-    const resolve = await axios.get('https://api.github.com/users'); // get data
+  //   const resolve = await axios.get(
+  //     `https://api.github.com/users?client_id=${
+  //       process.env.REACT_APP_GITHUB_CLIENT_ID
+  //     }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   ); // get data
+
+  //   //after getting data we update the state
+  //   this.setState({ users: resolve.data, loading: false });
+  // }
+
+  //Search github users
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const resolve = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    ); // get data
 
     //after getting data we update the state
-    this.setState({ users: resolve.data, loading: false });
-  }
+    this.setState({ users: resolve.data.items, loading: false });
+  };
 
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
